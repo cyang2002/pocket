72d02e280b2eec68c06e00ba990aa0eacb582ea3
Christopher Yang <96670935+cyang2002@users.noreply.github.com>
2026-03-27 21:03:47 -0500

    test

--- frontend/src/components/grid/CardTile.tsx
+++ frontend/src/components/grid/CardTile.tsx
@@ -4,7 +4,7 @@ import { formatIssuer, formatCategory, rateColorClass, getTopRates } from '@/lib
 import { BizBadge } from '@/components/ui/biz-badge'
 import type { CardGridItem } from '@/types/api'
 
-interface CardTileProps {
+interface CardTileProps { //tile
   card: CardGridItem
   inWallet: boolean
   onAdd: () => void
